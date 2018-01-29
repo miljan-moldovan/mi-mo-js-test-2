521e2955a97c544052d8b0b8d2cddff6e8c937ee
miljan-moldovan <miljan.moldovan@gmail.com>
2018-01-29 12:00:37 -0300

    Walkin screen ui fixes

--- screens/walkinScreen/WalkInScreen.js
+++ screens/walkinScreen/WalkInScreen.js
@@ -1,17 +1,64 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Image } from 'react-native';
+
+import rightArrow from '../../assets/images/walkinScreen/icon_arrow_right_xs.png';
+import serchImage from '../../assets/images/walkinScreen/icon_search_w.png';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#f3f3f4',
   },
   titleContainer: {
     backgroundColor: '#f3f3f4',
     flex: 7,
+    justifyContent: 'center',
+    paddingLeft: 20,
   },
   inputContainer: {
     flex: 10,
     backgroundColor: '#fff',
+    justifyContent: 'center',
+    paddingLeft: 20,
+    borderBottomColor: '#1D1D2626',
+    borderBottomWidth: 1,
+  },
+  listItemContainer: {
+    flex: 10,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingLeft: 20,
+    paddingRight: 20,
+    borderBottomColor: '#1D1D2626',
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+  },
+  btnContainer: {
+    flex: 10,
+    backgroundColor: '#D9D9D9',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  title: {
+    fontFamily: 'OpenSans-Bold',
+    fontSize: 12,
+    color: '#000',
+    letterSpacing: 1,
+  },
+  textInput: {
+    fontFamily: 'OpenSans-Regular',
+    fontSize: 18,
+    color: '#000',
+  },
+  textBtn: {
+    color: '#fff',
+    fontSize: 20,
+    fontFamily: 'OpenSans-Regular',
+  },
+  searchImage: {
+    tintColor: '#231F20',
   },
 });
 
@@ -20,37 +67,41 @@ class WalkInScreen extends Component {
     console.log('test');
   }
 
+  renderTitle = title => (
+    <View style={styles.titleContainer}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  )
+
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.container}>
-          <View style={styles.titleContainer}>
-            <Text>Cancel</Text>
-          </View>
-          <View style={styles.inputContainer}>
-            <Text>Cancel</Text>
-          </View>
-          <View style={styles.inputContainer}>
-            <Text>Cancel</Text>
-          </View>
-          <View style={styles.inputContainer}>
-            <Text>Cancel</Text>
-          </View>
-          <View style={styles.titleContainer}>
-            <Text>Cancel</Text>
-          </View>
-          <View style={styles.inputContainer}>
-            <Text>Cancel</Text>
-          </View>
-          <View style={styles.inputContainer}>
-            <Text>Cancel</Text>
-          </View>
-          <View style={styles.inputContainer}>
-            <Text>Cancel</Text>
-          </View>
-          <View style={styles.inputContainer}>
-            <Text>Cancel</Text>
-          </View>
+        {this.renderTitle('CLIENT INFO')}
+        <View style={styles.listItemContainer}>
+          <Text style={styles.textInput}>Client</Text>
+          <Image style={styles.searchImage} source={serchImage} />
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.textInput}>Email</Text>
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.textInput}>Phone</Text>
+        </View>
+        {this.renderTitle('SERVICE')}
+        <View style={styles.listItemContainer}>
+          <Text style={styles.textInput}>Service</Text>
+          <Image source={rightArrow} />
+        </View>
+        <View style={styles.listItemContainer}>
+          <Text style={styles.textInput}>Provider</Text>
+          <Image source={rightArrow} />
+        </View>
+        <View style={styles.listItemContainer}>
+          <Text style={styles.textInput}>Promo</Text>
+          <Image source={rightArrow} />
+        </View>
+        <View style={styles.btnContainer}>
+          <Text style={styles.textBtn}>ADD TO QUEUE</Text>
         </View>
       </View>
     );
