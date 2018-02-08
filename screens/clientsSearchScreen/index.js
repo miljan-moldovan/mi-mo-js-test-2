e9087c02b5f0af5a5a1ae348ccb9f20daf23ebf7
miljan-moldovan <miljan.moldovan@gmail.com>
2018-02-08 17:58:53 -0300

    Worked with the client walkin nav

--- screens/clientsSearchScreen/index.js
+++ screens/clientsSearchScreen/index.js
@@ -2,13 +2,16 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import clientsSearchActions from '../../actions/clientsSearch';
 import ClientsSearchScreen from './ClientsSearchScreen';
+import walkInActions from '../../actions/walkIn';
 
 const mapStateToProps = state => ({
   clientsSearchState: state.clientsSearchReducer,
+  walkInState: state.walkInReducer,
 });
 
 const mapActionsToProps = dispatch => ({
   clientsSearchActions: bindActionCreators({ ...clientsSearchActions }, dispatch),
+  walkInActions: bindActionCreators({ ...walkInActions }, dispatch),
 });
 
 export default connect(mapStateToProps, mapActionsToProps)(ClientsSearchScreen);
