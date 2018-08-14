8ee71eb8b3b2182732093dd4408f182bccaaf642
miljan-moldovan <miljan.moldovan@gmail.com>
2018-08-14 18:54:01 -0300

    Removed inline styles and added proptypes

---
+++ screens/blockTimeScreen/styles.js
@@ -0,0 +1,54 @@
+import {
+  StyleSheet,
+} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F1F1F1',
+  },
+  innerRow: {
+    flexDirection: 'row',
+    height: 44,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: '#C0C1C6',
+    paddingLeft: 16,
+    alignItems: 'center',
+    paddingRight: 16,
+  },
+  carretIcon: {
+    fontSize: 20,
+    color: '#727A8F',
+  },
+  contentStyle: { alignItems: 'flex-start', paddingLeft: 16 },
+  sectionTitle: {
+    fontSize: 14,
+    lineHeight: 22,
+    color: '#110A24',
+    fontFamily: 'Roboto',
+    marginLeft: 0,
+    marginTop: 7,
+  },
+  rightButtonText: {
+    fontSize: 14,
+    fontFamily: 'Roboto',
+    backgroundColor: 'transparent',
+    textAlign: 'center',
+  },
+  sectionDivider: { height: 37 },
+  inputGroup: { marginTop: 16 },
+  activityIndicator: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  cancelButton: { fontSize: 14, color: 'white' },
+  leftButtonText: { fontSize: 14, color: 'white' },
+  titleContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titleText: {
+    fontFamily: 'Roboto-Medium',
+    fontSize: 17,
+    lineHeight: 22,
+    color: 'white',
+  },
+});
