0f2363dce57328397b87b29ce717efbe92145a5c
miljan-moldovan <miljan.moldovan@gmail.com>
2018-08-17 13:05:27 -0300

    Fixed reference to wrong obj

--- screens/walkinScreen/components/WalkInScreen.js
+++ screens/walkinScreen/components/WalkInScreen.js
@@ -197,7 +197,6 @@ class WalkInScreen extends Component {
         serviceId: service.id,
       };
       this.props.walkInActions.postWalkinClient(params).then(() => {
-
         this.saving = false;
         this.props.navigation.navigate('Main');
       });
@@ -231,7 +230,7 @@ class WalkInScreen extends Component {
 
   render() {
     const fullName = this.getFullName();
-    const email = this.state.client && this.state.client.email ? this.client.email : '';
+    const email = this.state.client && this.state.client.email ? this.state.client.email : '';
     const phones = this.state.client && this.state.client.phones.map(elem => (elem.value ? elem.value : null)).filter(val => val).join(', ');
     return (
       <ScrollView style={styles.container}>
