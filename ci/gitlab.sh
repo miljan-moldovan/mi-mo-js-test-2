973f5ff5fba6ef1568981ab006047bf9f9d58ba2
miljan-moldovan <miljan.moldovan@gmail.com>
2018-10-12 13:57:45 +0300

    corrected build scripts

--- ci/gitlab.sh
+++ ci/gitlab.sh
@@ -4,32 +4,11 @@ set -eo pipefail
 
 printenv
 
-## for debugging
-# export SG_EXTRA_CONFIG='{
-# "parts": {
-#   "api": {
-#     "source": {
-#       "docker": {
-#         "tag": "17222"
-#       }
-#     }
-#   }
-# }
-# }'
-# export SG_PARTS=' api '
-# export SG_FLAVOR=bb
-# export SG_BRANCH=develop
-# export CI_ENVIRONMENT_NAME=dev
-# export CI_JOB_TOKEN=531fd76a147d8fa05d8357ad203782
-
-#echo ${SG_BRANCH:=develop} > /dev/null
-
 cat <<END
   SG has been triggered and will use the following args:
-    - SG_EXTRA_CONFIG=$SG_EXTRA_CONFIG
-    - SG_PARTS=$SG_PARTS
-    - SG_FLAVOR=$SG_FLAVOR
-    - SG_BRANCH=$SG_BRANCH
-    - CI_ENVIRONMENT_NAME=$CI_ENVIRONMENT_NAME
-    - CI_JOB_TOKEN=$CI_JOB_TOKEN
+    - CI_BUILD_REF=$CI_BUILD_REF
+    - AC_API_TOKEN=$AC_API_TOKEN
+    - AC_OWNER_NAME=$AC_OWNER_NAME
+    - AC_APP_NAME=$AC_APP_NAME
+    - CI_COMMIT_REF_NAME=$CI_COMMIT_REF_NAME
 END
