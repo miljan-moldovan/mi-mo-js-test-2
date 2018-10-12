9d2a226bd6f23cc5676904c59a9fb1261c65ce9e
miljan-moldovan <miljan.moldovan@gmail.com>
2018-10-12 14:54:41 +0300

    script

--- ci/gitlab.sh
+++ ci/gitlab.sh
@@ -3,15 +3,13 @@
 set -eo pipefail
 [ "$TRACE" = "true" ] && set -x
 
-printenv
-
 cat <<END
-  SG has been triggered and will use the following args:
-    - CI_BUILD_REF=$CI_BUILD_REF
-    - CI_COMMIT_REF_NAME=$CI_COMMIT_REF_NAME
-    - AC_API_TOKEN=$AC_API_TOKEN
-    - AC_OWNER_NAME=$AC_OWNER_NAME
-    - AC_APP_NAME=$AC_APP_NAME
+SG has been triggered and will use the following args:
+  - CI_BUILD_REF=$CI_BUILD_REF
+  - CI_COMMIT_REF_NAME=$CI_COMMIT_REF_NAME
+  - AC_API_TOKEN=$AC_API_TOKEN
+  - AC_OWNER_NAME=$AC_OWNER_NAME
+  - AC_APP_NAME=$AC_APP_NAME
 END
 
 json_value() {
@@ -65,3 +63,4 @@ while true; do
       ;;
   esac
 done
+
