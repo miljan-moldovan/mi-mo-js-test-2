af6d4b357af95fade62b28479250b66d42cb0482
miljan-moldovan <miljan.moldovan@gmail.com>
2018-11-01 22:11:29 -0300

    Fixed dependencies

--- components/formHelpers/index.js
+++ components/formHelpers/index.js
@@ -11,7 +11,7 @@ import {
 import PropTypes from 'prop-types';
 import { get, isString } from 'lodash';
 import FontAwesome, { Icons } from 'react-native-fontawesome';
-import TextInputMask from 'react-native-text-input-mask';
+import TextInputMask from '../TextInputMask';
 import ValidatableInput from './components/ValidatableInput';
 import ClientInput from './components/ClientInput';
 import InputDate from './components/InputDate';
