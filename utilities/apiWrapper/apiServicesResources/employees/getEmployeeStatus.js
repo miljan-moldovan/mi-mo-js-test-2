02e12fbf703bbb8ccfb125bbf8362de732fdcfa4
miljan-moldovan <miljan.moldovan@gmail.com>
2018-11-07 16:11:06 -0300

    IP-766

--- utilities/apiWrapper/apiServicesResources/employees/getEmployeeStatus.js
+++ utilities/apiWrapper/apiServicesResources/employees/getEmployeeStatus.js
@@ -4,13 +4,12 @@ import { getApiInstance } from '../../api';
 
 let cancellationToken = null;
 
-export default async (id) => {
+export default async (id, setCancelToken = true) => {
   const apiInstance = await getApiInstance();
   cancelRequest(cancellationToken);
 
-  return apiInstance.get(`Employees/${id}/Status`, {
-    cancelToken: new axios.CancelToken((c) => {
-      cancellationToken = c;
-    }),
-  }).then(({ data: { response } }) => response);
+  let cancelToken = { cancelToken: new axios.CancelToken((c) => { cancellationToken = c; }) };
+  cancelToken = setCancelToken ? cancelToken : {};
+
+  return apiInstance.get(`Employees/${id}/Status`, cancelToken).then(({ data: { response } }) => response);
 };
