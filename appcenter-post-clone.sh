6a531fdb509662b555c574f9393dd296d1ba9879
miljan-moldovan <miljan.moldovan@gmail.com>
2018-12-20 01:08:48 +0300

    Remove untracked files

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,5 +1,2 @@
-watchman watch-del-all
-rm -rf node_modules
-rm -rf $TMPDIR/react-*
-npm start -- --reset-cache
-rm -rf $TMPDIR/haste-map-react-native-packager-*
+git clean -xdf
+git reset HEAD --hard
