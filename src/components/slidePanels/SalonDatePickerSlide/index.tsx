e03acf7546e2f78edb3657e2d032ad0640c48f82
miljan-moldovan <miljan.moldovan@gmail.com>
2018-12-22 16:38:59 +0300

    remove space

--- src/components/slidePanels/SalonDatePickerSlide/index.tsx
+++ src/components/slidePanels/SalonDatePickerSlide/index.tsx
@@ -149,7 +149,7 @@ export default class SalonDatePickerSlide extends React.Component<IProps, IState
     direction === 'left'
       ? <Icon name="chevronLeft" size={12.5} color="#727a8f" type="solid" />
       : <Icon name="chevronRight" size={12.5} color="#727a8f" type="solid" />;
-      
+
   render() {
     return (
       <ModalBox
