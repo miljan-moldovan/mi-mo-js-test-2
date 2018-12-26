734174e00345e471c8cfb56b2d4e3ee3438c7472
miljan-moldovan <miljan.moldovan@gmail.com>
2018-12-26 23:02:32 +0300

    Upload build to crashlytics only for develop

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-./ios/Pods/Crashlytics/submit 57ed7a29664d211a062ac7a41c806fc3aeec24f2 069f729bbab573001180f1688cd1671414faa52c8894ba7b656b668bb4a52ecc \
+if [ "$APPCENTER_BRANCH" == "develop" ];
+then
+  echo "Uploading to Crashlytics"
+  ./ios/Pods/Crashlytics/submit 57ed7a29664d211a062ac7a41c806fc3aeec24f2 069f729bbab573001180f1688cd1671414faa52c8894ba7b656b668bb4a52ecc \
   -groupAliases beta_testers \
   -notifications YES \
   -ipaPath $APPCENTER_OUTPUT_DIRECTORY/SalonUltimateRN.ipa
+else
+    echo "Current branch is $APPCENTER_BRANCH"
+fi
