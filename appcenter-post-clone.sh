c31067338df0ea8fdbfebb605201bf70218a7448
miljan-moldovan <miljan.moldovan@gmail.com>
2018-12-26 19:16:35 +0300

    Remove node 6 and install 8

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,6 +1,9 @@
+#!/usr/bin/env bash
+set -ex
 git clean -xdf
 git reset HEAD --hard
-echo $PATH
-which node
-brew install nvm
-/usr/local/opt/nvm ls
+
+brew uninstall node@6
+NODE_VERSION="8.3.0"
+curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
+sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
