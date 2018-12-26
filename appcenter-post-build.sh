070904452182922c406991639e38d420c47f44f1
miljan-moldovan <miljan.moldovan@gmail.com>
2018-12-26 23:21:49 +0300

    Added Slack notification

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -7,6 +7,8 @@ then
   -groupAliases beta_testers \
   -notifications YES \
   -ipaPath $APPCENTER_OUTPUT_DIRECTORY/SalonUltimateRN.ipa
+  
+  curl -X POST -H 'Content-type: application/json' --data '{"text": "Available new build", "attachments": [{"color": "#36a64f","title": "Mobile POS","title_link": "https://betas.to/5k1zsCBG","actions": [{"type": "button","text": "Install","url": "https://betas.to/5k1zsCBG"}]}]}' https://hooks.slack.com/services/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 #su_mobileapp
 else
     echo "Current branch is $APPCENTER_BRANCH"
 fi
