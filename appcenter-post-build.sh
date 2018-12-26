4acaedac9281d57f876c196c79fa83d9226e0277
miljan-moldovan <miljan.moldovan@gmail.com>
2018-12-26 22:43:13 +0300

    Fixed typo in post build script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -3,4 +3,4 @@
 ./ios/Pods/Crashlytics/submit 57ed7a29664d211a062ac7a41c806fc3aeec24f2 069f729bbab573001180f1688cd1671414faa52c8894ba7b656b668bb4a52ecc \
   -groupAliases beta_testers \
   -notifications YES \
-  -ipaPath @$APPCENTER_OUTPUT_DIRECTORY/SalonUltimateRN.ipa
+  -ipaPath $APPCENTER_OUTPUT_DIRECTORY/SalonUltimateRN.ipa
