d9abbac3632476e106a9584ba01d39b0307ba5a2
miljan-moldovan <miljan.moldovan@gmail.com>
2019-01-02 09:58:41 +0300

    remove unnecessary code

--- src/screens/appointmentCalendarScreen/helpers.ts
+++ src/screens/appointmentCalendarScreen/helpers.ts
@@ -1,7 +1,7 @@
 export const getHeaderRoomsOrResources = (arr, type) => {
   const newArray = [];
 
-  [...arr].forEach((item) => {
+  arr.forEach((item) => {
     for (let i = 1; i <= item[`${type}Count`]; i++) {
 
       newArray.push({ ...item, name: `${item.name} #${i}`, id: `${item.id}_${i}` });
